4091fd77f1b4e19c3ffc8f242580e34c142d974c
irsayvid <dsdarimisetti@gmail.com>
2021-12-23 12:50:03 +0530

    remove drf related files

--- ytsearchapi/ytsearch/paginations.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import pagination
-
-class CustomPagination(pagination.PageNumberPagination):
-    page_size = 15
-    page_size_query_param = 'page_size'
-    max_page_size = 150
-    page_query_param = 'p'
--- ytsearchapi/ytsearchapi/settings.py
+++ ytsearchapi/ytsearchapi/settings.py
@@ -71,16 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ytsearchapi.wsgi.application'
 
-# Rest Framework settings
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-      'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 15,
-}
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
